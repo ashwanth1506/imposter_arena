ffad03261690d3d1f157206cc8b390acf5ca6e9c
ashwanth1506 <ashwanthkumar830@gmail.com>
2026-03-20 23:03:41 +0530

    Update Room.jsx

--- frontend/imposter/src/Room.jsx
+++ frontend/imposter/src/Room.jsx
@@ -7,7 +7,7 @@ import "./room.css";
 import StarsBackground from "./components/StarsBackground";
 
 
-const socket = io("http://localhost:3000");
+const socket = io("https://imposter-arena.onrender.com");
 
 function Room() {
   const sendSound = new Audio(sendMp3);
