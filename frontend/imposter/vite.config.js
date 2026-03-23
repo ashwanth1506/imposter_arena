6d7b1f6b9959637b15756adc8d18d2799f7751c7
ashwanth1506 <ashwanthkumar830@gmail.com>
2026-03-23 10:28:54 +0530

    Update vite.config.js

--- frontend/imposter/vite.config.js
+++ frontend/imposter/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    cssCodeSplit: false,
-  },
-  server: {
-    port: 5173,
-    strictPort: false,
-  },
 })
+
