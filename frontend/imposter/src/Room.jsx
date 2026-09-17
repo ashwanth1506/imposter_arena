09710ceefd8fdbbe6a96e9c8661398f863ec9c0e
/ashwanth1506 <ashwanthkumar830@gmail.com>
2026-09-17 18:42:16 +0530

    url

--- frontend/imposter/src/Room.jsx
+++ frontend/imposter/src/Room.jsx
@@ -8,7 +8,7 @@ import StarsBackground from "./components/StarsBackground";
 
 
 
-const socket = io("http://localhost:3000/", {
+const socket = io("https://imposter-arena.onrender.com", {
   transports: ["websocket"]
 });
 
